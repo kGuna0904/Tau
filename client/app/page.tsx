c13228d6185039be4506f07f9193b755793354f8
kGuna0904 <k.guna.90205@gmail.com>
2026-09-26 01:46:02 +0530

    hero home set

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,9 +1,14 @@
 import LoginPage from "./login/page";
+import Image from 'next/image';
 
 export default function Home() {
   return (   
     <>
-      <LoginPage />
+    <div className="relative w-full h-[110vh] overflow-hidden">
+      <Image src="/assets/images/wordmarked.png" className='absolute left-18 top-15' alt="Tau Logo" width={100} height={60}/>
+      <img src="/assets/images/hero_home.png" alt=""/>
+      <a href="https://tau-fincorp.vercel.app/login" className="bottom-89 left-20 px-10 py-3 absolute homeEffect ">Track now</a>
+    </div>  
     </>
   );
 }
